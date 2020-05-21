ed3fdc83d15d5c2a626777c3cf3e54a059560d6d
ChristinaGaitan <christina.gaitan@gmail.com>
2020-05-21 15:50:59 -0500

    Start directions for driver and change marker color for requester

--- app/src/main/java/com/lcgt/uberclonedemo/DriverLocationActivity.java
+++ app/src/main/java/com/lcgt/uberclonedemo/DriverLocationActivity.java
@@ -3,23 +3,82 @@ package com.lcgt.uberclonedemo;
 import androidx.fragment.app.FragmentActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class DriverLocationActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    Intent intent;
+    Double driverLatitude;
+    Double driverLongitude;
+    Double requestLatitude;
+    Double requestLongitude;
+
+    public void acceptRequest(View view) {
+        String requestingUsername = intent.getStringExtra("username");
+        final String driverUsername = ParseUser.getCurrentUser().getUsername();
+
+        ParseQuery<ParseObject> query = ParseQuery.getQuery("Request");
+        query.whereEqualTo("username", requestingUsername);
+
+        query.findInBackground(new FindCallback<ParseObject>() {
+            @Override
+            public void done(List<ParseObject> objects, ParseException e) {
+                if(e == null) {
+                    if(objects.size() > 0) {
+
+                        for(ParseObject object : objects) {
+                            object.put("driverUsername", driverUsername);
+
+                            object.saveInBackground(new SaveCallback() {
+                                @Override
+                                public void done(ParseException e) {
+                                    if(e == null) {
+                                        startDirections();
+                                    }
+                                }
+                            });
+                        }
+
+                    }
+                }
+            }
+        });
+    }
+
+    public void startDirections() {
+        String saddr = driverLatitude + "," + driverLongitude;
+        String daddr = requestLatitude + "," + requestLongitude;
+
+        Intent directionsIntent = new Intent(
+            android.content.Intent.ACTION_VIEW,
+            Uri.parse("http://maps.google.com/maps?saddr="+ saddr +"&daddr=" + daddr)
+        );
+        startActivity(directionsIntent);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,19 +104,27 @@ public class DriverLocationActivity extends FragmentActivity implements OnMapRea
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        Intent intent = getIntent();
+        intent = getIntent();
 
-        Double driverLatitude = intent.getDoubleExtra("driverLatitude", 0);
-        Double driverLongitude = intent.getDoubleExtra("driverLongitude", 0);
+        driverLatitude = intent.getDoubleExtra("driverLatitude", 0);
+        driverLongitude = intent.getDoubleExtra("driverLongitude", 0);
         LatLng driverLocation = new LatLng(driverLatitude, driverLongitude);
 
-        Double requestLatitude = intent.getDoubleExtra("requestLatitude", 0);
-        Double requestLongitude = intent.getDoubleExtra("requestLongitude", 0);
+        requestLatitude = intent.getDoubleExtra("requestLatitude", 0);
+        requestLongitude = intent.getDoubleExtra("requestLongitude", 0);
         LatLng requestLocation = new LatLng(requestLatitude, requestLongitude);
 
         ArrayList<Marker> markers = new ArrayList<>();
-        markers.add(mMap.addMarker(new MarkerOptions().position(driverLocation).title("Your location")));
-        markers.add(mMap.addMarker(new MarkerOptions().position(requestLocation).title("Request location")));
+        MarkerOptions driverMarkerOptions = new MarkerOptions().
+            position(driverLocation).
+            title("Your location");
+        MarkerOptions requesterMarkerOptions = new MarkerOptions().
+            position(requestLocation).
+            title("Request location").
+            icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE));
+
+        markers.add(mMap.addMarker(driverMarkerOptions));
+        markers.add(mMap.addMarker(requesterMarkerOptions));
 
         LatLngBounds.Builder builder = new LatLngBounds.Builder();
         for(Marker marker : markers) {
